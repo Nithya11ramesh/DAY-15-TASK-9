c07d2768e67d28e1aaef531649ca6df956f36b3d
Nithya11ramesh <nithyanisha062gmail.com>
2024-04-01 21:21:30 +0530

    call back task

---
+++ script.js
@@ -0,0 +1,40 @@
+const CallBacktask = document.getElementById("callBack")
+setTimeout(()=>{
+    CallBacktask.innerText='10'
+},1000)
+setTimeout(()=>{
+     CallBacktask.innerText='9'
+    },2000)
+ setTimeout(()=>{
+     CallBacktask.innerText='8'
+        },3000)
+setTimeout(()=>{
+     CallBacktask.innerText='7'
+        },4000)
+setTimeout(()=>{
+     CallBacktask.innerText='6'
+   },5000)
+setTimeout(()=>{
+     CallBacktask.innerText='5'
+   },6000)
+setTimeout(()=>{
+    CallBacktask.innerText='4'
+   },7000)
+setTimeout(()=>{
+    CallBacktask.innerText='3'
+    },8000)
+ setTimeout(()=>{
+     CallBacktask.innerText='2'
+      },9000)
+                                
+setTimeout(()=>{
+     CallBacktask.innerText='1'
+     },10000)
+setTimeout(()=>{
+    CallBacktask.innerText='Happy Independence Day'
+  },11000)
+                                   
+                       
+                            
+                    
+        
